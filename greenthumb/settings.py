b49f98166108373d873dd6220b2f772f1c71e14f
ant07hony <ant07hony@yahoo.com>
2023-04-19 15:34:37 -0400

    installed decouple

--- greenthumb/settings.py
+++ greenthumb/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--^=x--#3epn%zm&l7u7d@m74@(hv@)q)c@pir7=iph8^slc&0l'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
